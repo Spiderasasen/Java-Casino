3403cd640219f372de5b36b03ea565ceee2e03d7
Diego-Diaz797 <ddiaz11@elon.edu>
2025-09-02 10:02:09 -0400

    Creating the main screen Here we will make the main screen for the user it will have 1. the main title 2. a selection called "play"    () once selected it will show the 5 games that can be played 3. and an exit button
    Just added the error checks and also adding the main tital and also adding the slection area(part 1)

--- Main.java
+++ Main.java
@@ -1,7 +1,43 @@
 //imports
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //calling the scanner
+        Scanner scan = new Scanner(System.in);
+
+        while(true){
+            //checking if the input is an integer
+            try {
+                //printing the main title
+                System.out.println("̲\uD835\uDE79̲\uD835\uDE8A̲\uD835\uDE9F̲\uD835\uDE8A ̲\uD835\uDE72̲\uD835\uDE8A̲\uD835\uDE9C̲\uD835\uDE92̲\uD835\uDE97̲\uD835\uDE98");
+
+                //placing the 2 items and asking the user to make a selection
+                System.out.println("1. Play\n" +
+                        "2. Exit\n" +
+                        "Please select an option");
+                int choice = scan.nextInt();
+
+                //checking what choice the user slected
+                switch (choice){
+                    case 1:
+                        break;
+                    case 2:
+                        System.exit(0);
+                    default:
+                        System.out.println("Please enter a valid number that is in the menu");
+                }
+            }
+            //if there is another error that occurred
+            catch (InputMismatchException e){
+                System.out.println("Please enter a valid number");
+                String empty = scan.next();
+            }
+            catch (Exception e){
+                System.out.println("Error Occurred: " + e);
+                System.exit(0);
+            }
+        }
     }
 }
